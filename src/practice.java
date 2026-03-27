004253bf45aa823aa35cf44b6e8e8d5832417015
skyx016 <akashvshelke007@gmail.com>
2026-03-27 23:18:59 +0530

    practice

---
+++ src/practice.java
@@ -0,0 +1,10 @@
+public class practice {
+    static void main() {
+        int a = 11;
+        if (a == 10) {
+            System.out.println("i am not 11");
+        } else {
+            System.out.println("i am a 11");
+        }
+    }
+}
