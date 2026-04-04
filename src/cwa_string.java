488a6b61a8c07058d74a4db66a263848ded3641d
skyx016 <akashvshelke007@gmail.com>
2026-04-05 00:08:14 +0530

    loops

---
+++ src/cwa_string.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+public class string {
+    static void main() {
+        Scanner sc = new Scanner(System.in);
+        string st = sc.next();
+        System.out.println(st);
+    }
+}
