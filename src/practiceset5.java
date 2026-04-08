dab4fe84a9f544bb7341d1a3eba9ce02c73deda9
skyx016 <akashvshelke007@gmail.com>
2026-04-08 23:28:17 +0530

    practiceset5

---
+++ src/practiceset5.java
@@ -0,0 +1,49 @@
+
+public class practiceset5 {
+    static void main(String[] args) {
+        /*
+
+        // Practice problem 1
+        float [] Marks  = {12.4f,44.5f, 50.6f,44.5f };
+        float sum = 0;
+        for (float element : Marks){
+            sum = sum + element;
+        }
+        System.out.println("the value of sum is " + sum);
+
+         */
+        //problem number 2
+        /*
+        int [] points ={100 , 55 ,34 , 44, 55};
+        int  num = 100;
+        boolean isInarray = false;
+        for (int element : points){
+            if ( num == element){
+                isInarray = true;
+                break;
+            }
+        }
+if (isInarray){
+    System.out.println("the element is in array");
+}else {
+    System.out.println("the element not present in array");
+}
+
+
+         */
+
+        //problem  number 3
+        /*
+        int [] marks = { 88,40 , 56,58,99};
+        int sum = 0;
+        for (int element : marks){
+            sum = sum + element;
+        }
+        System.out.println("The average number in array is " + sum/ marks.length);
+
+
+         */
+        // problem number 4
+
+    }
+}
